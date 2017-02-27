8258822d7e7aeaf2bfb1d59fc9ddb1cd4582b9c2
Wilder <wilderbravo2000@gmail.com>
2017-02-27 13:25:32 +0000

    Pixel Art

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//Write your function calls below this line
+setColor(0, 0, "red");
+setColor(0, 9, "red");
+setColor(9, 0, "red");
+setColor(9, 9, "red");
+
+setColor(2, 2, "red");
+setColor(2,3, "red");
+setColor(2, 4, "red");
+setColor(3, 2, "red");
+setColor(4, 2, "red");
+setColor(4, 3, "red");
+setColor(4, 4, "red");
+setColor(5, 4, "red");
+setColor(6, 4,"red");
+setColor(6, 2, "red");
+setColor(6, 3, "red");
